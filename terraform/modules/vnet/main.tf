8b4fb41ea72dc5bd05a4e15a8503a5a39668994d
Fredrik Klingenberg <fredrkl@gmail.com>
2024-01-04 06:43:41 +0100

    feat: wrong CIDR block
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/vnet/main.tf
+++ terraform/modules/vnet/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  address_space       = ["10.0.0.1/16"]
+  address_space       = ["10.0.0.0/16"]
   location            = "eastus"
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
