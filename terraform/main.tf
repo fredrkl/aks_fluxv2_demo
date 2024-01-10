e0acab8309565dfddc206ccfee14513bf054aa7c
Fredrik Klingenberg <fredrkl@gmail.com>
2024-01-10 11:44:18 +0100

    feat: using northeurope region
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
-  location = "eastus"
+  location = "northeurope"
 }
 
 module "aks" {
